5fda1295706b362d7733364c10b1c32338a32505
ebouhid <eduardobouhid.neto@gmail.com>
2023-10-12 16:31:49 -0300

    Included necessary imports for loss declaration.

--- train.py
+++ train.py
@@ -1,10 +1,12 @@
 from binaryclassificationnet import BinaryClassificationNet
+from binaryinception import BinaryInception
 from dataset import GenderRecognitionDataset
 from torch.utils.data import DataLoader
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import EarlyStopping
+import torch.nn as nn
 
 BATCH_SIZE = 64
 NUM_WORKERS = 8
@@ -12,6 +14,8 @@ NUM_EPOCHS = 100
 CSV_DIR = 'faces.csv'
 MIN_DELTA = 1e-4
 PATIENCE = 5
+LEARNING_RATE = 5e-4
+LOSS = nn.BCEWithLogitsLoss()
 
 # Set random seed
 seed = 42
@@ -30,7 +34,8 @@ train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, nu
 val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=NUM_WORKERS)
 
 # Create model
-model = BinaryClassificationNet()
+# model = BinaryClassificationNet(loss=LOSS, lr=LEARNING_RATE)
+model = BinaryInception( lr=LEARNING_RATE)
 
 # Create early stopping callback
 early_stopping = EarlyStopping('val_loss', min_delta=MIN_DELTA,patience=PATIENCE)
